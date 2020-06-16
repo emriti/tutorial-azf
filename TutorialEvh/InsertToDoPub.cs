f6c247fc257222b938f0ae86cc6f2a0f52406cb4
emriti <emriti.gmail.com>
2020-06-16 14:19:05 +0700

    tutorial evh

---
+++ TutorialEvh/InsertToDoPub.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TutorialAZF.TutorialEvh
+{
+    public static class InsertToDoPub
+    {
+        [FunctionName("InsertToDoPub")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            [EventHub("insert-todo", Connection = "insert-todo-conn")] IAsyncCollector<string> outputEvents,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            log.LogInformation($"Receive http request to post data into event hub, body" + requestBody);
+            try
+            {
+                //ToDoItem toDoItem = JsonConvert.DeserializeObject<ToDoItem>(requestBody);
+                //await outputEvents.AddAsync(JsonConvert.SerializeObject(toDoItem));
+                //return (ActionResult)new OkObjectResult(new ResponseDTO((int) HttpStatusCode.OK, $"Successfully sending item to event hub, item with id {toDoItem.Id}"));
+
+                throw new Exception("Test12345");
+            }
+            catch (Exception e)
+            {
+                return (ActionResult)new BadRequestObjectResult(e.Message);
+            }
+        }
+    }
+}
---
+++ TutorialEvh/InsertToDoSub2.cs
@@ -0,0 +1,56 @@
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TutorialAZF.Models;
+
+namespace TutorialAZF.TutorialEvh
+{
+    public static class InsertToDoSub2
+    {
+        [FunctionName("InsertToDoSub2")]
+        public static async Task Run(
+            [EventHubTrigger("insert-todo", Connection = "insert-todo-conn", ConsumerGroup = "A")] EventData[] events,
+            [CosmosDB(
+                databaseName: "ToDoList",
+                collectionName: "Items",
+                ConnectionStringSetting = "CosmosDBConnection"
+            )] DocumentClient client,
+            ILogger log)
+        {
+            var exceptions = new List<Exception>();
+
+            foreach (EventData eventData in events)
+            {
+                try
+                {
+                    string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+                    log.LogInformation($"C# Event Hub trigger function processed a message: {messageBody}");
+                    ToDoItem toDoItem = JsonConvert.DeserializeObject<ToDoItem>(messageBody);
+                    toDoItem.Id = toDoItem.Id + "_Sub2";
+                    await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri("ToDoList", "Items"), toDoItem);
+                }
+                catch (Exception e)
+                {
+                    // We need to keep processing the rest of the batch - capture this exception and continue.
+                    // Also, consider capturing details of the message that failed processing so it can be processed again later.
+                    exceptions.Add(e);
+                }
+            }
+
+            // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that there is a record of the failure.
+
+            if (exceptions.Count > 1)
+                throw new AggregateException(exceptions);
+
+            if (exceptions.Count == 1)
+                throw exceptions.Single();
+        }
+    }
+}
